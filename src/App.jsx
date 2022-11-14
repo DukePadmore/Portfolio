3e87b041aeae06b4b80beb9bb8429440615d9ef8
Duke Padmore <dukepadmore@gmail.com>
2022-11-14 16:09:38 +0100

    Organize Sass files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import './App.scss';
+import './sass/main.scss';
 import Header from './components/Header';
 import Intro from './components/Intro';
 import About from './components/sections/About';
@@ -9,7 +9,7 @@ import Contact from './components/sections/Contact';
 import Experience from './components/sections/Experience';
 import Projects from './components/sections/Projects';
 import Footer from './components/Footer';
-import Loading from './Pages/Loading';
+import Loading from './components/Loading';
 
 gsap.registerPlugin(ScrollTrigger);
 
