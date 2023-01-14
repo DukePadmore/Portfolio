d568431d0698e8cbccb94261b9e440685d10872c
Duke Padmore <dukepadmore@gmail.com>
2023-01-14 10:32:55 -0500

    update CV & about section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -5,9 +5,9 @@ function About() {
         <h2 className='about__title'>About Me</h2>
         <p className='about__desc'>
           Hi ! My name is Duke and I am a French junior front-end developer who
-          will soon be relocating to Canada. After almost 10 years of experience
-          in Retail and Procurement, I decided to transition to a new career in
-          a field that has fascinated me for many years now: Web Development.
+          recently moved to Canada. After almost 10 years of experience in
+          Retail and Procurement, I decided to transition to a new career in a
+          field that has fascinated me for many years now: Web Development.
         </p>
         <p className='about__desc'>
           Thanks to an intensive 6-months Fullstack JavaScript program at École
